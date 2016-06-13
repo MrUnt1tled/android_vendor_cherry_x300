43bc09937aea204daff0547a3e91ce68f2e6bbff
John Reinel Caluag <johnrenielcaluag1009@gmail.com>
2016-06-13 14:10:20 +0800

    Update to x300

---
+++ x300-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherry/x300/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherry/x300/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherry/x300/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherry/x300/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherry/x300/proprietary/vendor,system/vendor)
